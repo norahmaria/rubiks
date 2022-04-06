4c3c923cc912213ffb854be1d26deb3f133b9078
Norah Maria Hareide Nilsen <norahmariano@gmail.com>
2022-04-06 18:05:59 +0200

    Vite Config Build

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr-default-export-option'
 
 export default defineConfig({
+  build: { outDir: 'build' },
+  base: '/rubiks/',
   plugins: [react(), svgr({ svgr: { icon: true, expandProps: 'end' } })],
 })
